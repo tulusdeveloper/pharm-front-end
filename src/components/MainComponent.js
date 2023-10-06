1eca9eb10003ff0c307b21a9263b40e76833e1f6
tulusdeveloper <tulusdeveloper@gmail.com>
2023-10-07 02:52:42 +0300

    changes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,16 +4,17 @@ import PageLoader from './PageLoader';
 import Navbar from './Navbar';
 import HomeComponent from './HomeComponent';
 import Sidebar from './Sidebar';
+import "adminbsb-materialdesign/css/themes/all-themes.css";
 
 
 class MainComponent extends React.Component {
 	render() {
+		document.getElementById("root").className="theme-red";
 		return <React.Fragment>
-			<Overlay />
-			<Navbar />
-			<Sidebar />
-			<HomeComponent />
-            <PageLoader />
+			<Overlay/>
+			<Navbar/>
+			<Sidebar/>
+			<HomeComponent/>
 		</React.Fragment>;
 	};
 }
