70f617978e77739eac38a27745cbb5e3dd2a44d2
tulusdeveloper <tulusdeveloper@gmail.com>
2023-10-07 12:35:34 +0300

    started adding sidebar responsiveness

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,13 +8,34 @@ import "adminbsb-materialdesign/css/themes/all-themes.css";
 
 
 class MainComponent extends React.Component {
+
+	state={
+		bodyClass: "theme-red ls-closed",
+
+	}
+	onBarClick=()=>{
+		if(this.state.bodyClass=="theme-red ls-closed overlay-open"){
+			this.setState({bodyClass:"theme-red ls-closed"})
+		}
+		else if(this.state.bodyClass=="theme-red ls-closed"){
+			this.setState({bodyClass:"theme-red ls-closed overlay-open"})
+		}
+	};
+
 	render() {
-		document.getElementById("root").className="theme-red";
+		if (window.screen.width > 1150) {
+			document.getElementById("root").className = "theme-red";
+		}
+		else {
+			document.getElementById("root").className = this.state.bodyClass;
+		}
+
 		return <React.Fragment>
-			<Overlay/>
-			<Navbar/>
-			<Sidebar/>
-			<HomeComponent/>
+			<Overlay />
+			<Navbar onBarClick={this.onBarClick}/>
+			<Sidebar />
+			<HomeComponent />
+			<PageLoader />
 		</React.Fragment>;
 	};
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,7 @@ class Navbar extends React.Component {
 		return <nav className="navbar">
 			<div className="container-fluid">
 				<div className="navbar-header">
-					<a href="#" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
-					<a href="#" className="bars" style={{ display: "none" }}></a>
+					<a href="#" className="bars" onClick={this.props.onBarClick}></a>
 					<a className="navbar-brand" href="#">ADMINBSB - MATERIAL DESIGN</a>
 				</div>
 
